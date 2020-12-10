7715a5dd65cb565fca50a518da4a823c1ec54b74
abigail huerta <abi_1816@hotmail.com>
2020-12-10 11:23:15 -0600

    Add project files.

---
+++ DemoNet5/Models/Photo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DemoNet5.Models
+{
+    public class Photo
+    {
+        public int id { get; set; }
+        public string author { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+        public string url { get; set; }
+        public string download_url { get; set; }
+    }
+}
